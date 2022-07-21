cfb5b1c105ed0c45729eb7175d0d03900a543296
leemoispace <leemoispace@gmail.com>
2022-07-21 11:06:19 +0400

    P2 Landing Page (#2)
    * starter files.
    
    * build the nav html with js
    
    * add nav style.
    
    * add scroll smooth function.
    
    * landing page final version.
    
    * solved the review points.
    
    * sectionID change to scrollTarget to make sure consistant.
    
    Co-authored-by: leemoispace <>

--- p2-landing-page/js/app.js
+++ p2-landing-page/js/app.js
@@ -22,33 +22,56 @@
  * Define Global Variables
  *
  */
+const sectionNavs = document.querySelectorAll("section");
+const navTag = document.getElementById("navbar__list");
+const sectionNavLength = sectionNavs.length;
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
+// 1.Build the nav get position info array
+sectionNavs.forEach((element, index) => {
+  const sectionName = element.getAttribute("data-nav");
+  const scrollTarget = element.offsetTop;
+  const liTag = document.createElement("li");
+  liTag.setAttribute("class", "nav" + index);
+  liTag.innerHTML = `<a onClick="scrollToTargets(${scrollTarget})">${sectionName}</a>`;
+  navTag.appendChild(liTag);
+});
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+// 2.Scroll to targets ID using scrollTO event
+function scrollToTargets(scrollTarget) {
+  window.scrollTo({ left: 0, top: scrollTarget, behavior: "smooth" });
+}
 
-// build the nav
+// 3.Add class 'active' to section when near top of viewport
+document.addEventListener("scroll", () => {
+  const sectionNavPositions = [];
+  // update all navs positions for every pixal scroll,
+  // before the +50 is my test for the boundary conditions.test for the boundary changing moments considering the nav bar height.if dont need to control that accurate the experience dont differ.
+  sectionNavs.forEach((element) =>
+    sectionNavPositions.push(element.getBoundingClientRect().top)
+  );
+  const addIndex = sectionNavPositions.findIndex((element) => element > 0);
+  for (let i = 0; i < sectionNavLength; i++) {
+    if (addIndex === i) {
+      document.querySelector(".nav" + addIndex).classList.add("active");
+      document
+        .querySelector(`#section${addIndex + 1}`)
+        .classList.add("current-active-class");
+    } else {
+      document.querySelector(".nav" + i).classList.remove("active");
+      document
+        .querySelector(`#section${i + 1}`)
+        .removeAttribute("current-active-class");
+    }
+  }
+});
 
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+// findIndex can solve below, below codes not useful.just keep a record.
+// currentPosition = this.scrollY;
+// // moving window to find find which section r we in
+// allSectionPositions=[0,...sectionNavPositions,document.body.scrollHeight];
+// while(currentPosition>allSectionPositions[currentPositionIndex]){
+//     currentPositionIndex++;
+//     if(currentPosition<allSectionPositions[currentPositionIndex]){
+//         document.querySelector(".nav" + parseInt(currentPositionIndex-1)).classList.add("active");
+// 		//document.querySelector(`#section${currentPositionIndex + 1}`).classList.add("current-active-class");
+//         break;
