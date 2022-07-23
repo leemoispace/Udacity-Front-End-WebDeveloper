975a4c7c2fcad441000e8b37b56a4ef05b372cb0
leemoispace <>
2022-07-24 01:05:10 +0400

    protect API key private and safe.

--- p4-evaluate-news-nlp/src/server/index.js
+++ p4-evaluate-news-nlp/src/server/index.js
@@ -1,6 +1,15 @@
+
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
+const dotenv = require("dotenv");
+
+// Set up API key.
+dotenv.config();
+api_key = {
+  api: process.env.API_KEY
+}
+
 
 const app = express();
 
