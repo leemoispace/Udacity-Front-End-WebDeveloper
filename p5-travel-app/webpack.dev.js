38d16d0b83fba7d80b7b1c9b77d604cca5965656
leemoispace <>
2022-07-29 17:22:19 +0400

    setup webpack dev

--- p5-travel-app/webpack.dev.js
+++ p5-travel-app/webpack.dev.js
@@ -3,17 +3,54 @@
 
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "development",
-  module: {
-    rules: [],
+  devtool: "source-map",
+  stats: "verbose",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
   },
-  devServer: {
-    
+  devServer: {},
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|jpg|gif|webp)$/,
+        loader: "file-loader",
+      },
+    ],
   },
   plugins: [
-
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
   ],
 };
