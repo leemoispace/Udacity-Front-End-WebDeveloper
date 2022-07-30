ce1c158d4f75ba7553d19a4a96077e073a284556
leemoispace <>
2022-07-30 22:02:29 +0400

    event listener for search button and Api related functions

--- p5-travel-app/src/client/index.js
+++ p5-travel-app/src/client/index.js
@@ -3,14 +3,40 @@
 // styles referenced in html/css
 
 //import functions
+import {getGeoCity} from './js/geoNamesApi';
+import {getWeatherForecast} from './js/weatherApi';
+import { getCity, getTripStart, getTripEnd } from "./js/form";
 
 //import style
 //import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.scss";
 import "bootstrap";
-
 const $ = require("jquery");
 
-//export functions
+const trip = {};
+
+//functions
+const handleSearch = async (e) => {
+    e.preventDefault();
+  
+    trip.city = getCity();
+    trip.start = getTripStart();
+    trip.end = getTripEnd();
+  
+    const geocity = await getGeoCity(trip.city);
+  
+    trip.latitude = geocity.latitude;
+    trip.longitude = geocity.longitude;
+    trip.countryCode = geocity.countryCode;
+  
+    trip.weatherForecast = await getWeatherForecast(
+      geocity.latitude,
+      geocity.longitude
+    );
+
 
 //add event listeners
+/* Add event listeners */
+document
+  .getElementById("button_search")
+  .addEventListener("click", handleSearch);
